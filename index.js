099d83b1795a367209f57fe3ec50e9f80e1520dd
MariaEduardaSeichas <oliveiramariaeduarda3478@gmail.com>
2022-08-17 15:08:23 -0400

    commit feito

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+//const { usuario } = require('./models');
+
+const app = express();
+
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+
+app.get('/', async function(req, res){
+    var resultado = "teste"
+    res.json(resultado);
+  //var resultado = await usuario.findAll();
+  //res.json(resultado);
+});
+
+app.get('/autenticar', async function (req, res){
+    res.render("autenticar")
+});
+
+app.post('/logar', async function (req, res){
+    
+});
+
+app.post('/deslogar', async function (req, res){
+    
+});
+app.listen(3000, function() {
+  console.log('App de Exemplo escutando na porta 3000!')
+});
